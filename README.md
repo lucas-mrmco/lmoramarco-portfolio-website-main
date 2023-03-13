8f0ac2174815d2f0171d301d31a1e2c67b0f0248
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2023-03-13 10:15:39 +0100

    v1 without contact -link# -qqs erreurs de css

---
+++ README.md
@@ -0,0 +1,11 @@
+# Lucas Moramarco -  portfolio mode designer
+
+A portfolio web site completely made in html css and js from ground up.
+
+#### Découvrir le rendu ici (https://designer.lucasmoramarco.fr/)
+
+Vous avez également la possibilité de visonner mon portfolio de 2 autres façons.
+
+#### Une version plus formelle ici (https://lucasmoramarco.fr/)
+#### Une version plus amusabte ici (https://explore.lucasmoramarco.fr/)
+
---
+++ assets/js/main.js
@@ -0,0 +1,311 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SERVICES MODAL ===============*/
+// Get the modal
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
+
+// When the user clicks on the button, open the modal
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+let mixer = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+/* Link active work */
+const workLinks = document.querySelectorAll(".work__item");
+
+function activeWork(workLink) {
+  workLinks.forEach((wl) => {
+    wl.classList.remove("active-work");
+  });
+  workLink.classList.add("active-work");
+}
+
+workLinks.forEach((wl) => {
+  wl.addEventListener("click", () => {
+    activeWork(wl);
+  });
+});
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*=============== LIGHT DARK THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const lightTheme = "light-theme";
+const iconTheme = "bx-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(lightTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    lightTheme
+  );
+  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the light / icon theme
+  document.body.classList.toggle(lightTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal(`.nav__menu`, {
+  delay: 100,
+  scale: 0.1,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(`.home__data`);
+sr.reveal(`.home__handle`, {
+  delay: 100,
+});
+
+sr.reveal(`.home__social, .home__scroll`, {
+  delay: 100,
+  origin: "bottom",
+});
+
+sr.reveal(`.about__img`, {
+  delay: 100,
+  origin: "left",
+  scale: 0.9,
+  distance: "30px",
+});
+
+sr.reveal(`.about__data, .about__description, .about__button-contact`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "right",
+  distance: "30px",
+});
+
+sr.reveal(`.skills__content`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "bottom",
+  distance: "30px",
+});
+
+sr.reveal(`.services__title, services__button`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "top",
+  distance: "30px",
+});
+
+sr.reveal(`.work__card`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "bottom",
+  distance: "30px",
+});
+
+sr.reveal(`.testimonial__container`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "bottom",
+  distance: "30px",
+});
+
+sr.reveal(`.contact__info, .contact__title-info`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "left",
+  distance: "30px",
+});
+
+sr.reveal(`.contact__form, .contact__title-form`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "right",
+  distance: "30px",
+});
+
+sr.reveal(`.footer, footer__container`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "bottom",
+  distance: "30px",
+});
+
+
+
+
+
+
+/* Please ❤ this if you like it! */
+
+
+(function ($) {
+  "use strict";
+
+
+  //Parallax & fade on scroll	
+
+  function scrollBanner() {
+    $(document).on('scroll', function () {
+      var scrollPos = $(this).scrollTop();
+      $('.parallax-fade-top').css({
+        'top': (scrollPos / 2.8) + 'px',
+        'opacity': 1 - (scrollPos / 600)
+      });
+    });
+  }
+  scrollBanner();
+
+  //Page cursors
+
+  document.getElementsByTagName("body")[0].addEventListener("mousemove", function (n) {
+    t.style.left = n.clientX + "px",
+      t.style.top = n.clientY + "px",
+      e.style.left = n.clientX + "px",
+      e.style.top = n.clientY + "px",
+      i.style.left = n.clientX + "px",
+      i.style.top = n.clientY + "px"
+  });
+  var t = document.getElementById("cursor"),
+    e = document.getElementById("cursor2"),
+    i = document.getElementById("cursor3");
+  function n(t) {
+    e.classList.add("hover"), i.classList.add("hover")
+  }
+  function s(t) {
+    e.classList.remove("hover"), i.classList.remove("hover")
+  }
+  s();
+  for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+    o(r[a])
+  }
+  function o(t) {
+    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+  }
+
+  $(document).ready(function () {
+    "use strict";
+
+    //Scroll indicator
+
+    var progressPath = document.querySelector('.progress-wrap path');
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+    var updateProgress = function () {
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progress = pathLength - (scroll * pathLength / height);
+      progressPath.style.strokeDashoffset = progress;
+    }
+    updateProgress();
+    $(window).scroll(updateProgress);
+
+
+  });
+
+})(jQuery); 
